656ba1694dec59675706f711e9b7122e45d3cffd
seyda <iclalseyda@gmail.com>
2022-07-22 11:43:33 +0200

    Logout page

--- src/test/java/com/intabella/pages/LogoutPage.java
+++ src/test/java/com/intabella/pages/LogoutPage.java
@@ -1,4 +1,27 @@
 package com.intabella.pages;
 
+import com.intabella.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LogoutPage {
+
+    public LogoutPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
+    @FindBy(xpath = "//*[@id=\"user-menu\"]/a")
+    public WebElement userMenu;
+
+    @FindBy(xpath = "//*[@id=\"user-menu\"]/ul/li[4]/a")
+    public WebElement LogoutButton;
+
+
+
+    public void LogoutFunction() {
+        userMenu.click();
+        LogoutButton.click();
+    }
 }
