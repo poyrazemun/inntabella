a3c37af2f7d98b4f987c4682f9b6c946799c174a
poyrazemun <ofatih-poyraz@hotmail.com>
2022-07-22 20:36:00 +0200

    Step Definitions created.

--- src/test/java/com/intabella/runners/CukesRunner.java
+++ src/test/java/com/intabella/runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/intabella/step_definitions",
-        dryRun = true,
+        dryRun = false,
         tags = ""
 )
 
